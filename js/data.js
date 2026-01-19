519131b7871875e7e7a816f9edd9f0f718cabec5
Celdrone <mikhaelmoniagabusiness@gmail.com>
2026-01-19 23:06:46 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,13 +1,13 @@
 let shipments = [
   {
-    id: 'LX-204981',
+    id: 'TX-204981',
     from: 'Jakarta Hub',
     to: 'Surabaya Hub',
     eta: '18 Jan 2026',
     status: 3
   },
   {
-    id: 'LX-772310',
+    id: 'TX-772310',
     from: 'Bandung Hub',
     to: 'Bali Hub',
     eta: '20 Jan 2026',
